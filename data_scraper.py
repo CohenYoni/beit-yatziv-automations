2f0168b5bed33802a314b3cd92cc74e1ac1213f2
Yoni Cohen <yonicohen1993@gmail.com>
2020-11-21 17:57:14 +0200

    get phonebook of students method && refactor to safe dictionary actions

--- data_scraper.py
+++ data_scraper.py
@@ -185,31 +185,35 @@ class MashovScraper:
         self._session.close()
         self._logged_in = False
 
-    def create_behavior_report_by_dates(self, from_date: date, to_date: date, class_code: str) -> dict:
+    def get_behavior_report_by_dates(self, from_date: date, to_date: date, class_code: str) -> pd.DataFrame:
         def parse_json_res(res_obj):
+            student_json = res_obj.get('student', {})
+            lesson_log_json = res_obj.get('lessonLog', {})
+            teacher_json = res_obj.get('teacher', {})
+            achva_json = res_obj.get('achva', {})
+            achva_remark_json = res_obj.get('achvaRemark', {})
+            justified_by_json = res_obj.get('justifiedBy', {})
+            achva_justification_json = res_obj.get('achvaJustification', {})
+            lesson_date = lesson_log_json.get('lessonDate', '')
+            date_format = '%Y-%m-%dT%H:%M:%S'
+            try:
+                lesson_date = datetime.strptime(lesson_date, date_format).strftime('%d/%m/%Y')
+            except:
+                print(f'Warning: Lesson date format changed! ("{lesson_date}" instead of {date_format})')
             required_data = [
-                res_obj['teacher']['teacherName'],
-                res_obj['subjectName'],
-                datetime.strptime(res_obj['lessonLog']['lessonDate'], '%Y-%m-%dT%H:%M:%S').strftime('%d/%m/%Y'),
-                res_obj['lessonLog']['lesson'],
-                res_obj['student']['studentId'],
-                f'{res_obj["student"]["familyName"]} {res_obj["student"]["privateName"]}',
-                res_obj['student']['classCode'],
-                res_obj['student']['classNum'],
-                res_obj['achva']['name']
+                teacher_json.get('teacherName', ''),
+                res_obj.get('subjectName', ''),
+                lesson_date,
+                lesson_log_json.get('lesson', ''),
+                student_json.get('studentId', ''),
+                f'{student_json.get("familyName", "")} {student_json.get("privateName", "")}',
+                student_json.get('classCode', ''),
+                student_json.get('classNum', ''),
+                achva_json.get('name', ''),
+                achva_remark_json.get('remarkText', ''),
+                justified_by_json.get('teacherName', ''),
+                achva_justification_json.get('justification', '')
             ]
-            try:
-                required_data.append(res_obj['achvaRemark']['remarkText'])
-            except KeyError:
-                required_data.append('')
-            try:
-                required_data.append(res_obj['justifiedBy']['teacherName'])
-            except KeyError:
-                required_data.append('')
-            try:
-                required_data.append(res_obj['achvaJustification']['justification'])
-            except KeyError:
-                required_data.append('')
             return required_data
 
         encoded_class = urllib.parse.quote(class_code)
@@ -221,5 +225,80 @@ class MashovScraper:
         columns = ['teacher_name', 'subject', 'lesson_date', 'lesson_num', 'student_id', 'student_name', 'class_code',
                    'class_num', 'event_type', 'remark', 'justified_by', 'justification']
         data = [parse_json_res(v) for v in json_res]
-        df = pd.DataFrame(data, columns=columns)
-        return df
+        behavior_report_df = pd.DataFrame(data, columns=columns)
+        return behavior_report_df
+
+    def get_phonebook_of_students(self, class_code: str) -> pd.DataFrame:
+        def parse_json_res(details, extra_data):
+            student_json = details.get('student', {})
+            student_info_json = details.get('studentInfo', {})
+            contacts_list = details.get('contacts', [])
+            parent1_json = contacts_list[0] if len(contacts_list) > 0 else {}
+            parent2_json = contacts_list[1] if len(contacts_list) > 1 else {}
+            student_id = str(student_json.get('studentId', ''))
+            student_extra_data_json = extra_data.get(student_id, {})
+            date_format = '%Y-%m-%dT%H:%M:%S'
+            student_birthdate = student_json.get('birthDate', '')
+            try:
+                student_birthdate = datetime.strptime(student_birthdate, date_format).strftime('%d/%m/%Y')
+            except:
+                print(f'Warning: Student birthdate format changed! ("{student_birthdate}" instead of {date_format})')
+            required_data = [
+                student_id,
+                student_json.get('familyName', ''),
+                student_json.get('privateName', ''),
+                student_json.get('gender', ''),
+                student_json.get('classCode', ''),
+                student_json.get('classNum', ''),
+                student_birthdate,
+                student_json.get('hebrewBirthDate', ''),
+                student_json.get('major', ''),
+                student_info_json.get('city1', ''),
+                student_info_json.get('address1', ''),
+                student_info_json.get('city2', ''),
+                student_info_json.get('address2', ''),
+                student_info_json.get('phone1', ''),
+                student_info_json.get('email1', ''),
+                student_info_json.get('cellphone1', ''),
+                parent1_json.get('contact', {}).get('contactId', ''),
+                parent1_json.get('contact', {}).get('privateName', ''),
+                parent1_json.get('contactInfo', {}).get('email1', ''),
+                parent1_json.get('contactInfo', {}).get('cellphone1', ''),
+                parent2_json.get('contact', {}).get('contactId', ''),
+                parent2_json.get('contact', {}).get('privateName', ''),
+                parent2_json.get('contactInfo', {}).get('email1', ''),
+                parent2_json.get('contactInfo', {}).get('cellphone1', ''),
+                '',
+                student_extra_data_json.get('NoBro', ''),
+                student_extra_data_json.get('NoCom', ''),
+                student_extra_data_json.get('OrClass', ''),
+                student_extra_data_json.get('OrTeacher', ''),
+                student_extra_data_json.get('rama', ''),
+                student_extra_data_json.get('Sat', ''),
+                '',
+                ''
+            ]
+            return required_data
+
+        encoded_class = urllib.parse.quote(class_code)
+        details_url = f'{self.BASE_URL}/api/classes/{encoded_class}/students/details'
+        details_res = self._session.get(details_url, headers={'Referer': self.MAIN_DASHBOARD_PAGE_URL})
+        json_details_res = details_res.json()
+        extra_data_url = f'{self.BASE_URL}/api/classes/{encoded_class}/students/extraData'
+        extra_data_res = self._session.get(extra_data_url, headers={'Referer': self.MAIN_DASHBOARD_PAGE_URL})
+        json_extra_data_res = extra_data_res.json()
+        for _id, det_list in json_extra_data_res.items():
+            new_dict = dict()
+            for el in det_list:
+                if 'columnName' in el:
+                    new_dict[el['columnName']] = el.get('val', '')
+            json_extra_data_res[_id] = new_dict
+        columns = ['student_id', 'family_name', 'private_name', 'gender', 'class_code', 'class_num',
+                   'birthdate', 'heb_birthdate', 'study_trend', 'main_city', 'main_address', 'sec_city', 'sec_address',
+                   'home_phone', 'student_mail', 'student_phone_num', 'parent1_id', 'parent1_name', 'parent1_mail',
+                   'parent1_phone_num', 'parent2_id', 'parent2_name', 'parent2_mail', 'parent2_phone_num',
+                   'edge_means', 'num_brothers', 'num_computers', 'original_class', 'original_teacher', 'level',
+                   'saturday_practitioner', 'material_help', 'home_visits']
+        data = [parse_json_res(v, json_extra_data_res) for v in json_details_res]
+        phonebook_df = pd.DataFrame(data, columns=columns)
+        return phonebook_df
